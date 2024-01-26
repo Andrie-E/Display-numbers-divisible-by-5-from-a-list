2c87abdcc4f9bc69f33a23ab4130a4605c5a60bd
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 17:06:11 +0800

    code for the pseudocodes

--- divisible_by_five.py
+++ divisible_by_five.py
@@ -3,6 +3,12 @@
 #pseudocode
 
 # provide list
+list = [10, 20, 33, 46, 55]
 # print
+print("the list of numbers are:", list)
+print('Divisible by 5:')
 # write the function
+for num in list:
+    if num % 5 == 0:
 # print outcome
+         print(num)
